0bfdbacf8117b49f63c26ab532fd8fd0cbcf61fd
Jesse Cooke <jesse@relativepath.io>
2019-05-27 16:27:49 -0700

    110 cols

--- config.h
+++ config.h
@@ -126,7 +126,7 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
+static unsigned int cols = 110;
 static unsigned int rows = 24;
 
 /*
